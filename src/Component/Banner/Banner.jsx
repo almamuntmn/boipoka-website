8a52ef1df7a498d3a110395aae403ccc4f5583a2
almamuntmn <almamuncool88@gmail.com>
2025-09-05 11:25:38 +0600

    banner edit

--- src/Component/Banner/Banner.jsx
+++ src/Component/Banner/Banner.jsx
@@ -2,14 +2,16 @@ import Bookimg from '../../assets/books.jpg'
 
 const Banner = () => {
     return (
-        <div className="hero bg-base-200 min-h-96 border border-gray-400 rounded-lg my-10">
-            <div className="hero-content flex-col lg:flex-row-reverse py-20 px-32">
+        <div className="hero bg-base-200 min-h-96 my-10">
+            <div className="hero-content flex-col lg:flex-row-reverse py-10 px-5 md:py-16 md:px-12 lg:py-20 lg:px-32">
                 <img
                     src={Bookimg}
-                    className="max-w-sm rounded-lg shadow-2xl"
+                    className="w-full max-w-xs md:max-w-sm rounded-lg shadow-2xl"
                 />
-                <div className='lg:mr-20'>
-                    <h1 className="text-5xl font-bold mb-5">Books to freshen up your bookshelf</h1>
+                <div className="mt-6 md:mt-0 lg:mr-20 text-center md:text-left">
+                    <h1 className="text-2xl md:text-4xl lg:text-5xl font-bold mb-5">
+                        Books to freshen up your bookshelf
+                    </h1>
                     <button className="btn bg-[#23BE0A]">View The List</button>
                 </div>
             </div>
